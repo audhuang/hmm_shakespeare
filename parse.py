44f9f3d9ef049c8b56f4eca2d72c35629c172bfc
ottery <audreynhuang@gmail.com>
2016-03-07 15:57:53 -0800

    initial parser
    converts each line to list of words and prints for now cause i don't know what to do with it

---
+++ parse.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from __future__ import division
+from scipy.integrate import quad
+import random 
+import numpy as np 
+# import matplotlib.pyplot as plt
+
+
+# just prints the line as a list of words for now because i'm not sure what to 
+# do with it. have to deal with punctuation, capitalization, etc. 
+def parse(): 
+	inp = open('./project2data/shakespeare.txt')
+
+	# stores list of words in the line in "words"
+	for line in inp: 
+		words = line.split()
+		print(words)
+
+if __name__ == '__main__':
+	parse()
+
+
+# between poems, the lines are [], [], [number]
