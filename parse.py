3a97727256df06bd3e4819874ce1524ad1936935
Audrey Huang <audreynhuang@gmail.com>
2016-03-11 07:17:39 -0800

    outputs dictionary of unique word : unique index

--- parse.py
+++ parse.py
@@ -3,20 +3,39 @@ from __future__ import division
 from scipy.integrate import quad
 import random 
 import numpy as np 
-# import matplotlib.pyplot as plt
+import codecs
+import string
+import re
 
+from nltk.corpus import cmudict
 
-# just prints the line as a list of words for now because i'm not sure what to 
-# do with it. have to deal with punctuation, capitalization, etc. 
+d = cmudict.dict() # dicionary of syllables from cmudict
+word_dic = {} # dictionary of unique word : unique index 
+
+# parses the text file 'shakespeare.txt' and adds each unique word to a dictionary,
+# WORD_DIC, with a unique index 
 def parse(): 
-	inp = open('shakespeare.txt')
 
-	# stores list of words in the line in "words"
-	for line in inp: 
-		words = line.split()
-		print(words)
+	# open 'shakespeare.txt'
+	inp = open('./project2data/shakespeare.txt', 'r')
+
+	# use regex to parse unique words into list 'unique'
+	text = inp.read().lower() # make everything lowercase
+	text = re.sub('[^a-z\ \'\-]+', ' ', text) # replace punctuation with spaces
+	words = list(text.split()) # split file into a list of words by spaces
+	unique = list(set(words)) # get unique list of words
+
+	# for each unique word, add as key to dictionary with unique index as value
+	i = 0
+	for word in unique: 
+		word_dic[word] = i
+		i += 1
+
+
+
+def syllables(): 
+	print(d['lour\'st'][0])
+
 
 if __name__ == '__main__':
 	parse()
-
-# between poems, the lines are [], [], [number]
